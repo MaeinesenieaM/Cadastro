56bf5e22e5d19a17b02c51cec960d85463d913a3
David .C Gomes <52863043+MaeinesenieaM@users.noreply.github.com>
2023-08-18 10:54:29 -0300

    Arquivo principal.

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main ()
+{
+   int idade;
+   char nome [51];
+   char matricula [16];
+   char endereco [91];
+   char curso [31];
+   int periodo;
+   int disciplinas;
+   float mensalidade;
+
+   setlocale (LC_ALL, "Portuguese");
+
+   printf ("Insire o seu nome: ");
+   fflush (stdin);
+   fgets (nome, 51, stdin);
+   printf ("Insira a sua idade: ");
+   scanf ("%d", &idade);
+   printf ("Sua matrícula : ");
+   scanf ("%s", matricula);
+   printf ("Seu endereço : ");
+   fflush (stdin);
+   fgets (endereco, 31, stdin);
+   printf ("O Curso atual : ");
+   fflush (stdin);
+   fgets (curso, 31, stdin);
+   printf ("Seu Período : ");
+   scanf ("%d", &periodo);
+   printf ("Quantas disciplinas esta cursando?: ");
+   scanf ("%d", &disciplinas);
+   printf ("Sua mensalidade: ");
+   scanf ("%f", &mensalidade);
+
+   printf (".\n.\n.\n.\nNome: %sIdade: %d \nMatricula: %s \nEndereco: %s \nCurso: %sPeríodo: %d \nDisciplinas: %d \nMensalidade: %f",
+   	        nome, idade, matricula, endereco, curso, periodo, disciplinas, mensalidade);
+   printf ("\n\n    __o  bici! \n  _/><_ \n (_)/(_) \n  ");
+
+   scanf ("%d", &idade);
+
+
+
+}
